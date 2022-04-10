f5e50ec1e4e196f86d40ab84ab28fe8879404aa6
ufrek <robert.ks.email@gmail.com>
2022-04-10 01:10:27 -0400

    added how to find stuff in database

--- backend/backEnd.js
+++ backend/backEnd.js
@@ -21,19 +21,31 @@ async function main()
         })*/
 
         //inserts 3 listings into the airbnb sample database
-        await createMultipleListings(client, [{
+        /*await createMultipleListings(client, [{
             name: "loveley beach house",
-            summary: "its on the beach"
+            summary: "its on the beach",
+            bedrooms: 3
         },
             {
                 name: "Corn Shack",
-                summary: "A house on a corn field"
+                summary: "A house on a corn field",
+                bedrooms: 1
             },
             {
                 name: "My Crappy Dorm",
-                summary: "This place is tiny"
-            }])
+                summary: "This place is tiny",
+                bedrooms: 2
+            }]);*/
 
+        //await findOneListingByName(client, "My Crappy Dorm")
+
+        //if the document is missing a value, like bathrooms, it is not included in the results
+        //after checking, if it lacks a field when you try to access it, it will display invalid value
+        await findListingsWithMinimumCriteria(client, {
+            minimumBedrooms: 0,
+            minimumBathrooms: 0,
+            maxNumberResults: 20
+        })
     }
     catch(e)
     {
@@ -85,3 +97,56 @@ async function createMultipleListings(client, listingsArray)
     console.log(result.insertedIds)
 }
 
+//returns only one document, even if there are multiple same documents in collection
+async function findOneListingByName(client, nameOfListing)
+{
+    const result = await client.db("sample_airbnb").collection("ListingsAndReviews").findOne({name: nameOfListing});
+
+    if(result)
+    {
+        console.log(`Found a listing in the collection with a name: '${nameOfListing}'`);
+        console.log(result);
+    }
+    else
+    {
+        console.log(`No listings found with name '${nameOfListing}'`);
+    }
+}
+
+//find results in a database with at leas the minimum bedrooms, bathrooms, and a maximum number of results
+//check documentation for other comparison query operators
+async function findListingsWithMinimumCriteria(client, {minimumBedrooms = 0,
+                                                        minimumBathrooms = 0,
+                                                        maxNumberResults = Number.MAX_SAFE_INTEGER } = {})
+{
+    //can iterate over cursor to get results one by one (check documentation)
+    const cursor = await client.db("sample_airbnb").collection("ListingsAndReviews").find({
+        bedrooms: {$gte: minimumBedrooms},
+        bathrooms: {$gte: minimumBathrooms},
+    }).sort({last_review : -1}).limit(maxNumberResults);
+    //sort by last review in descending order (most recent first)
+    //limits results to maxNumberResults
+
+    //returns all results as an array
+    const results = await  cursor.toArray();
+
+    //prints a summary of each listing
+    if(results.length >  0)
+    {
+       console.log(`Found listing(s) with at least ${minimumBedrooms} bedrooms and ${minimumBathrooms} bathrooms: `);
+       results.forEach((result, i) => {
+           let date = new Date(result.last_review).toDateString();
+           console.log();
+           console.log(`${i+1}. name: ${result.name}`);
+           console.log(`_id: ${result._id}`);
+           console.log(`Bedrooms: ${result.bedrooms}`);
+           console.log(`Bathrooms: ${result.bathrooms}`);
+           console.log(`Most Recent Review Date: ${new Date(result.last_review).toDateString()}`)
+
+    })
+    }
+    else
+    {
+        console.log(`No results found`);
+    }
+}
