c74e21e48ed5bd0ad50c7422635b4ed125e2dcf9
ufrek <robert.ks.email@gmail.com>
2022-04-16 00:57:09 -0400

    more test

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,19 @@ var express = require('express');
 const cron = require('node-cron');
 const shell = require('shelljs');
 
+//to get this working, run this script in pm2 start server.js when connected to server
 
 var app = express();
 
 let counter = 0;
-cron.schedule('03 22 * * * *', function(){
+cron.schedule('0 1 * * *', function(){
     counter = counter + 1;
     //console.log("scheduled task running: " + counter + " tiimes");
 })
 
 
 app.get('/', function(req, res) {
-    res.send('Hello World updated ' + counter + 'times');
+    res.send('Daily Midnight check updated ' + counter + 'times');
 })
 var server = app.listen(8080, function() {
     console.log("Backend Application listening at http://localhost:8080")
