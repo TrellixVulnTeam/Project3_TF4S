e068bc0a3545002f5cbbfeed35d228cc9f645f2d
FNeffati <56371425+FNeffati@users.noreply.github.com>
2022-05-20 23:13:23 -0400

    updted schedulerrrr

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -1,3 +1,5 @@
+import {PythonShell} from "python-shell";
+
 const cron = require('node-cron')
 const shell = require('shelljs')
 import { updateYoutubeDB } from './youTubeToDB' //used for changing from localHost to AWS use
@@ -13,14 +15,19 @@ import { updateYoutubeDB } from './youTubeToDB' //used for changing from localHo
 let counter = 0;
 
 //This code runs daily at 10:31 AM (for now)
-cron.schedule('31 10 * * * *', function(){
+cron.schedule('34 22 * * * *', function(){
     counter = counter + 1;
-    //console.log("scheduled task running: " + counter + " tiimes");
+    // I want to get the time when it updates and put it on the database
+    PythonShell.run('Butler.py', null, function (err, results) {
+        if (err) throw err;
+    });
+
+    console.log("scheduled task running: " + counter + " times");
 
     //uncomment to call update to youtubeDB
-    //updateYoutubeDB();
-},{
+    updateYoutubeDB();
+    },{
 
     timezone: "America/New_York" //easter time zone
 
-})
+    })
